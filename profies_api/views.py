795c6279002059245cb5c956a2cb02c199d5b0de
Abdul <abdul@Piyush-Priyadarshis-MacBook-Pro.local>
2020-08-06 14:44:01 +0530

    added post put patch ad delete message to api view

--- profies_api/views.py
+++ profies_api/views.py
@@ -1,9 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profies_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializers
+
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
@@ -14,3 +20,32 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': "hello", 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}.'
+            return Response({
+                'message': message
+            })
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        req = self.request
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update an object"""
+        return Response({'message': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'message': 'DELETE'})
